af2c3feaf1a75d79a893e89463f84a74a826f71f
Bobby Gibbs <bobby.gibbs@GIBBSBD1.hisc.local>
2015-07-29 13:24:51 -0500

    Changed auto-refresh to first look at file length

--- run.rb
+++ run.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'listen'
+require 'io/console'
 
 get "/" do 
     send_file 'main.html'
@@ -13,6 +13,10 @@ post "/actions/:msg" do |n|
     redirect back
 end
 
+get "/log" do
+    IO.read("log.txt").length.to_s
+end
+
 get "/:page" do |n|
     send_file n
-end
+end 
