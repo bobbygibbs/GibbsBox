9a1dfb39d1850091a6176a294cbe1f58e44bf1ca
Bobby Gibbs <bobbyG27@cox.net>
2015-12-29 00:10:17 -0600

    Added basic username associated login functionality

--- run.rb
+++ run.rb
@@ -1,15 +1,44 @@
 require 'sinatra'
 require 'io/console'
 
+enable :sessions
+
 get "/" do 
-    send_file 'main.html'
+    send_file 'login.html'
+end
+
+post "/login/:usr" do
+	usr = params[:username]
+	if !IO.read("usr.txt").split().include? usr then
+		redirect back
+	end
+	#open('usr.txt', 'a') do |f|
+	#	f << "\n" << usr
+	#end
+	session[:username] = usr
+	redirect to('/index')
+end
+
+get "/index" do
+	if !session[:username].nil? then
+		puts session[:username]
+		send_file 'main.html'
+	end
+	redirect to('/')
+end
+
+get "/logout" do
+	session[:username] = nil
+	redirect to('/')
 end
 
 post "/actions/:msg" do |n|
-    msg = params[:message]
-    open('log.txt', 'a') do |f|
-        f << "\n" << msg
-    end
+	if !session[:username].nil? then
+		msg = params[:message]
+		open('log.txt', 'a') do |f|
+			f << "\n" << session[:username] + ": " + msg
+		end
+	end
     redirect back
 end
 
